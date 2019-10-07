a3363298a3427e5629aa031fd609f51d3cd7de81
Chaejin Lim <icj0103@ajou.ac.kr>
2019-10-07 17:37:18 +0900

    roughly completed I processing part

--- assembler.c
+++ assembler.c
@@ -56,7 +56,7 @@ struct Sym{
 };
 
 struct Sym *Symbols;
-struct Text *Texts;
+struct Text* Texts;
 int datasize, textsize;
 
 /*
@@ -100,55 +100,103 @@ void read_asm () {
        if (strcmp(temp, ".text") == 0) {
 	    break;
        } else if(temp[strlen(temp)-1] == ':') {
-	    
-
+	   struct Sym* cur = Symbols;
+	   while (cur->next!=NULL)
+	   {
+	    cur = cur->next;
+	   }	       
+	   cur->next = temp_sym;
+	   temp_sym = (struct Sym*)malloc(sizeof(struct Sym));
+	   temp_sym->name = (char*)malloc(sizeof(char));
+	   strncpy(temp_sym->name, temp, strlen(temp)-1);
+	   temp_sym->address = address;
        } else if(strcmp(temp, ".word") == 0) {
-	
-           address += 4; 
+	   temp_data = (struct Data*)malloc(sizeof(struct Data));
+	   int input;	 scanf("%d", &input);
+	   temp_data->value = input;
+	   struct Data* cur = temp_sym->first;
+	   if (cur == NULL)
+	   {
+	    cur = temp_data;
+	    temp_sym->size += 1;
+	    address = address + 4;
+	   }else{
+	   while (cur->next!=NULL)
+	   {
+	      cur = cur->next;	     
+	   } 
+	   cur = temp_data;
+	   temp_sym->size += 1;
+           address = address + 4; 
+	   }
        }
     }
 
     datasize = address - 0x10000000;
-
+   
     //Read .text region
     address = 0x400000;
     for (i=0; scanf("%s", temp) == 1;) {
 
-       if (temp[strlen(temp)-1] == ':') {	
-	   address += 4;   
+       if (temp[strlen(temp)-1] == ':') {	   
+	    temp_sym = (struct Sym*)malloc(sizeof(struct Sym));
+	    temp_sym->name = (char*)malloc(sizeof(char));
+	    strncpy(temp_sym->name, temp, strlen(temp)-1);
+	    if (strcmp(temp_sym->name, "main")==0)
+		address += 4;
+	    temp_sym->address = address;
+	    struct Sym* cur = Symbols;
+	    while(cur->next!=NULL)
+	    {
+		cur = cur->next;	    
+	    }	
+	    cur->next = temp_sym;
        } else {
-	  temp_text = (struct Text*)malloc(sizeof(struct Text));
 	  for (int i = 0; i < 20; i++)
 	  {
 	    if (strcmp(inst[i].name, temp)==0)
 	    {
 		address += 4;
+		temp_text = (struct Text*)malloc(sizeof(struct Text));
 		if (inst[i].type == 'R')
 		{
 		 temp_text->idx = i;
-		 scanf("%s", temp_text->d);
-		 scanf("%s", temp_text->s); 
-	         scanf("%s", temp_text->t);
+		 temp_text->d = (char*)malloc(sizeof(char));
+		 temp_text->s = (char*)malloc(sizeof(char));
+		 temp_text->t = (char*)malloc(sizeof(char));
+		 scanf("%s", temp);
+		 strcpy(temp_text->d, temp);
+		 scanf("%s", temp);
+		 strcpy(temp_text->s, temp); 
+	         scanf("%s", temp);
+		 strcpy(temp_text->t, temp);
 		 temp_text->address = address;
 		 struct Text* cur = Texts;
 		 while (cur->next!=NULL)
-		 {
+		 { 
 		    cur = cur->next;
 		 }
-		 Texts->next = temp_text;
-		}
+		  cur->next = temp_text;
+		 }	
 		else if (inst[i].type == 'I')
 		{
+		 temp_text->s = (char*)malloc(sizeof(char));
+	         temp_text->t = (char*)malloc(sizeof(char));
+		 temp_text->d = (char*)malloc(sizeof(char));	 
 		 temp_text->idx = i;
-		 scanf("%s", temp_text->s);
-		 scanf("%s", temp_text->t);
+		 scanf("%s", temp);
+		 strcpy(temp_text->s, temp);
+		 scanf("%s", temp);
+		 strcpy(temp_text->t, temp);
+		 scanf("%s", temp);
+		 strcpy(temp_text->d, temp);
 		 temp_text->address = address;
 		 struct Text* cur = Texts;
 		 while (cur->next!=NULL)
 		 {
 		    cur = cur->next;
 		 }
-		 Texts->next = temp_text;
+		 cur->next = temp_text;
 		}
 		else if(inst[i].type == 'J')
 		{
@@ -159,45 +207,52 @@ void read_asm () {
 		 {
 		    cur = cur->next;
 		 }
-		 Texts->next = temp_text;
+		 cur->next = temp_text;
 		}
 	    }
-	  }  
-	  /* char mssg[33];
-	   int son = 0;
-	   for (int i = 0; i < 20; i++)
-	   {
-		if (strcmp(inst[i].name, temp) == 0)
+	  }
+	  if (strcmp("la", temp)==0)
+	    {
+		address += 4;
+		temp_text = (struct Text*)malloc(sizeof(struct Text));
+		temp_text->t = (char*)malloc(sizeof(char));
+		temp_text->s = (char*)malloc(sizeof(char));
+		temp_text->d = (char*)malloc(sizeof(char));
+		temp_text->idx = 9;
+		char abbos[0x1000], austin[0x1000];
+	        scanf("%s", abbos);
+		strcpy(temp_text->t, abbos);
+		temp_text->s = "0";
+		scanf("%s", austin);
+		strcpy(temp_text->d, austin);
+	        temp_text->address = address;
+		struct Text* cur = Texts;
+		while (cur->next!=NULL)
+                {
+                    cur = cur->next;
+                }
+                cur->next = temp_text;
+		    
+		address += 4;
+	        temp_text = (struct Text*)malloc(sizeof(struct Text));
+		temp_text->s = (char*)malloc(sizeof(char));
+		temp_text->t = (char*)malloc(sizeof(char));
+		temp_text->d = (char*)malloc(sizeof(char));
+		temp_text->idx = 13;
+	        strcpy(temp_text->t, abbos);
+		strcpy(temp_text->s, abbos);
+		strcpy(temp_text->d, austin);
+	        temp_text->address = address;
+		cur = Texts;
+		while (cur->next!=NULL)
 		{
-		    if (inst[i].type == 'R')
-		    {
-			for (son; son < 6; son++)
-			{
-			   mssg[son] =  inst[i].op[son];
-			}
-			for (son; son < 11; son++)
-			{
-			    char k[4];  scanf("%s", &k);
-
-			}
-					
-		    }
-		    else if (inst[i].type == 'I')
-		    {
-		    
-		    }
-		    else if (inst[i].type == 'J')
-		    {
-		    
-		    }
-		}
-	   }*/
+		    cur = cur->next;
+                }
+	        cur->next = temp_text;
+	  }  			
        }
     }
-
     textsize = address - 0x400000;
-    printf("%s\n", NumToBits(textsize, 32));
-    printf("%s\n", NumToBits(datasize, 32));
 }
 
 /*
@@ -208,10 +263,95 @@ void subst_asm_to_num () {
     struct Sym *sym;
 
     for (text = Texts->next; text != NULL; text = text->next) {
-        /* blank */
-    }
+       if (inst[text->idx].type == 'R')
+       {
+	  if (text->d[0] == '$')
+	  {
+	    char *ptr = strtok(text->d, "$,");
+	    strcpy(text->d, ptr);
+	  }
+          if (text->s[0] == '$')
+	  {
+	    char *ptr = strtok(text->s, "$,");
+	    strcpy(text->s, ptr);
+	  }
+	  if (text->t[0] == '$')
+          {
+	    char *ptr = strtok(text->t, "$,");
+	    strcpy(text->t, ptr);
+	  }	      
+       }
+       else if(inst[text->idx].type == 'I')
+       {
+	if (text->s[0] == '$')
+	{
+	   char *ptr = strtok(text->s, "$,");
+	   strcpy(text->s, ptr);
+	}
+        if (text->t[0] == '$')
+        {
+	   char *ptr = strtok(text->t, "$,");
+	   strcpy(text->t, ptr);
+	}
+	if (atoi(text->d) == 0)
+	{
+	  int check = -1;  
+	   for (sym = Symbols->next; sym != NULL; sym = sym->next) { 
+		if(strcmp(sym->name, text->d)==0)
+		{
+		    check = 1;
+		    if (strcmp("lui", inst[text->idx].name)==0)
+		    {			
+			char* plag = NumToBits(sym->address, 32);
+			strncpy(text->d, plag, 16);
+			int u = 0;
+			for (int k = 16; k < 32; k++)
+			{
+			    if (plag[k]=='0')
+			    {
+				u++;
+			    }
+			}
+			if (u == 16)
+			{
+			    text->next = text->next->next;
+			    break;
+			}			    
+		    }
+		    else if (strcmp("ori", inst[text->idx].name)==0)
+		    {
+			char* plag = NumToBits(sym->address, 32);
+			char dom[17] = {0, };
+			for (int i = 16, j = 0; i < 32; i++, j++)
+			{
+			    dom[j] = plag[i];
+			}
+			    strcpy(text->d, dom);
+		    }
+		    else
+		    {
+			int liv = sym->address - text->address;
+		        liv /= 4;
+			char buf[17];
+			sprintf(buf, "%d", liv);
+		        strcpy(text->d, buf);
+		    }
+		    break;
+		}
+	   }
+	   if (check == -1) 
+           {
+              if (text->d[0] == '0' && text->d[1] == 'x')
+	      {
+                  char *ptr1 = strtok(text->d, "x");
+		  ptr1 = strtok(NULL, "x");  	  
+                  strcpy(text->d, ptr1);
+	      }                                    
+           }
+	}	    
+       }	   
+    } 
 }
-
 
 /*
  * print the results of assemble
@@ -220,20 +360,31 @@ void print_bits () {
     struct Text* text;
     struct Sym* sym;
     struct Data* data;
-    
+
     // print the header
     printf("%s", NumToBits(textsize, 32));
     printf("%s", NumToBits(datasize, 32));
-
+    
     // print the body
     for (text = Texts->next ; text != NULL ; text = text->next) {
         printf("%s", inst[text->idx].op);
 
         if (inst[text->idx].type == 'R') {
-            /* blank */
+	    printf("%s", NumToBits(atoi(text->s), 5));
+	    printf("%s", NumToBits(atoi(text->t), 5));
+	    printf("%s", NumToBits(atoi(text->d), 5));
+	    printf("00000");
+            printf("%s", inst[text->idx].funct);
 
         } else if(inst[text->idx].type == 'I') {
-            /* blank */
+             printf("%s", NumToBits(atoi(text->s), 5));
+	     printf("%s", NumToBits(atoi(text->t), 5));
+	     if (strcmp(inst[text->idx].name, "lui") == 0 || strcmp(inst[text->idx].name, "ori")==0)
+	     {
+		printf("%s", text->d);
+	     }
+	     else		 
+		printf("%s", NumToBits(atoi(text->d), 16));
 
         } else if(inst[text->idx].type == 'S') {
             /* blank */
